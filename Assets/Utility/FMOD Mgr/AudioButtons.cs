89007587dd4c6f87704dd670b004526ab05f657e
Sean Lange <seanlange@The-Benatar.local>
2019-03-31 13:53:49 -0700

    fmod unity interface UI

---
+++ Assets/Utility/FMOD Mgr/AudioButtons.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioButtons : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(ToggleActive);
+    }
+
+    private void ToggleActive()
+    {
+        GetComponentInParent<AudioMaster>().ToggleActive();
+    }
+}
